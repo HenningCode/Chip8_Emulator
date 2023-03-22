d83204bdb185d5596e79bf3cd2ae5ff74f34ed84
HenningCode <henningwilmer@yahoo.de>
2023-03-22 14:46:55 +0100

    addded first instruction

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,48 @@
 use std::fs;
+use rand::Rng;
 
-#[allow(dead_code)]
-const START_ADDRESS:u16 = 0x200;
+const START_ADDRESS:usize = 0x200;
+const FONSET_START_ADDRESS:usize = 0x50;
+const FONTSET_SIZE: usize = 80;
+static FONTSET: [u8; FONTSET_SIZE] = [
+    0xF0, 0x90, 0x90, 0x90, 0xF0, // 0
+	0x20, 0x60, 0x20, 0x20, 0x70, // 1
+	0xF0, 0x10, 0xF0, 0x80, 0xF0, // 2
+	0xF0, 0x10, 0xF0, 0x10, 0xF0, // 3
+	0x90, 0x90, 0xF0, 0x10, 0x10, // 4
+	0xF0, 0x80, 0xF0, 0x10, 0xF0, // 5
+	0xF0, 0x80, 0xF0, 0x90, 0xF0, // 6
+	0xF0, 0x10, 0x20, 0x40, 0x40, // 7
+	0xF0, 0x90, 0xF0, 0x90, 0xF0, // 8
+	0xF0, 0x90, 0xF0, 0x10, 0xF0, // 9
+	0xF0, 0x90, 0xF0, 0x90, 0x90, // A
+	0xE0, 0x90, 0xE0, 0x90, 0xE0, // B
+	0xF0, 0x80, 0x80, 0x80, 0xF0, // C
+	0xE0, 0x90, 0x90, 0x90, 0xE0, // D
+	0xF0, 0x80, 0xF0, 0x80, 0xF0, // E
+	0xF0, 0x80, 0xF0, 0x80, 0x80  // F
+];
 
 #[allow(dead_code)]
 struct Chip8{
     registers: [u8; 16],
     memory: [u8; 4096],
     index: u16,
-    pc: u16,
+    pc: usize,
     stack: [u16; 16],
     sp: u8,
     delay_timer: u8,
     sound_timer: u8,
     keypad: [u8; 16],
     video: [u32; 64*32],
-    opcode: u16
+    opcode: u16,
 }
 
 #[allow(dead_code)]
 impl Chip8 {
     
     fn new() -> Self{
-        Self {
+        let mut new = Self {
             registers: [0; 16],
             memory: [0; 4096],
             index: 0,
@@ -34,7 +54,12 @@ impl Chip8 {
             keypad: [0; 16],
             video: [0; 64*32],
             opcode: 0
-            }
+            };
+            
+        for i in 1..FONTSET_SIZE{
+            new.memory[FONSET_START_ADDRESS + i] = FONTSET[i]
+        }
+        new
     }
    
     fn load_rom(&mut self, path: &str) -> Result<(), Box<dyn std::error::Error>>{
@@ -43,13 +68,21 @@ impl Chip8 {
         for i in 1..length {
             self.memory[START_ADDRESS as usize + i] = contents[i];
         }
-        // self.memory = fs::read(path)
         Ok(())
+    }
+    
+    // maybe this needs to be different 
+    fn rand_byte() -> i32 {
+        rand::thread_rng().gen_range(0..256)
+    }
+    
+    fn op_00e0(&mut self){
+        self.video.iter_mut().for_each(|m| *m = 0)
     }
 }
 
 
 fn main() {
-    let chip = Chip8::new();
+    //let chip = Chip8::new();
     println!("Hello, world!");
 }
